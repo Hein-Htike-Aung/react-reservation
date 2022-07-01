2a9cccc946161ef4a4704422c7197f10c6ffe0af
niziu1151998@gmail.com <niziu1151998@gmail.com>
2022-07-01 21:24:17 +0630

    commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Hotel from './pages/hotel/Hotel';
+import Hotels from './pages/hotels/Hotels';
+
+const App = () => {
+	return (
+		<Routes>
+			<Route path='/' element={<Home />} />
+			<Route path='/hotels' element={<Hotels />} />
+			<Route path='/hotel/:id' element={<Hotel />} />
+		</Routes>
+	);
+};
+
+export default App;
